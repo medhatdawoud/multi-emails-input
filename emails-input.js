9fe4a656a201044c9c7524d2135b435f2824c85b
Medhat Dawoud <mdaawoud@zvmb0086.home>
2020-06-05 13:23:20 +0200

    Fix design and remove the delete button from selection
    - add "more" conditionally to the input placeholder
    - stop probagation for clicking on the email block

--- emails-input.js
+++ emails-input.js
@@ -5,6 +5,16 @@ function isValidEmail(email) {
   return expression.test(email);
 }
 
+function checkAndFixInputPlaceHolder() {
+  const hasEmails = listOfEmails.length > 0;
+  const inputBox = document.querySelector('input.email-input');
+  if (hasEmails) {
+    inputBox.placeholder = 'add more people...';
+  } else {
+    inputBox.placeholder = 'add people...';
+  }
+}
+
 function addEmailToList(emailsContainer, email) {
   const emailBlock = document.createElement('span');
   emailBlock.classList.add('email-block');
@@ -12,15 +22,21 @@ function addEmailToList(emailsContainer, email) {
     emailBlock.classList.add('invalid-email');
   }
   emailBlock.innerText = email;
+  emailBlock.addEventListener('click', (e) => e.stopPropagation());
+
   const removeBtn = document.createElement('span');
   removeBtn.innerHTML = '&times;';
   removeBtn.classList.add('remove-button');
-  removeBtn.addEventListener('click', function (e) {
+  removeBtn.addEventListener('click', function () {
+    listOfEmails.splice(this.parentElement.innerText, 1);
     this.parentElement.remove();
+    checkAndFixInputPlaceHolder();
   });
   emailBlock.append(removeBtn);
+
   listOfEmails.push(email);
   emailsContainer.append(emailBlock);
+  checkAndFixInputPlaceHolder();
 }
 
 function EmailsInput(selector) {
@@ -28,7 +44,7 @@ function EmailsInput(selector) {
   const emailsContainer = document.createElement('span');
   const input = document.createElement('input');
   input.classList.add('email-input');
-  input.placeholder = `add more people...`;
+  input.placeholder = `add people...`;
   input.setAttribute('type', 'email');
 
   input.addEventListener('keypress', (e) => {
